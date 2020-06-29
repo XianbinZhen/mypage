f1d21a2be51225cf223ac52e8c7112cbce785277
xianbinzhen <xianbin.zhen001@umb.edu>
2020-06-28 20:53:15 -0400

    add modal

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,22 @@
 const leftBtn = document.querySelector("#left-btn");
 const rightBtn = document.querySelector("#right-btn");
-const intros = document.querySelectorAll(".introduction")
+const intros = document.querySelectorAll(".introduction");
+const skillBtn = document.querySelectorAll(".skill");
+const skillModal = document.querySelector("#modal-skill");
+const modalContent = document.querySelector(".modal-skill");
+const closeBtnTop = document.querySelector("#close-btn-top");
+const closeBtnBottom = document.querySelector("#close-btn-bottom");
 
-
+// handle intro bg change
 leftBtn.addEventListener("click", leftBtnClick);
 rightBtn.addEventListener("click", rightBtnClick);
+
+
+//handle skill button click
+skillBtn[0].addEventListener("click", skillBtnClick);
+skillModal.addEventListener("click", outSideClick);
+closeBtnBottom.addEventListener("click", closeSkillModal);
+closeBtnTop.addEventListener("click", closeSkillModal);
 
 function leftBtnClick(e) {
     let index = indexOfCurrent();
@@ -29,4 +41,19 @@ function indexOfCurrent() {
         }
     })
     return i;
+}
+
+
+function skillBtnClick(e) {
+    skillModal.classList.add("show-modal");
+}
+
+function closeSkillModal(e) {
+    skillModal.classList = "";
+}
+
+function outSideClick(e) {
+    if (e.target == skillModal) {
+        skillModal.classList = "";
+    }
 }
